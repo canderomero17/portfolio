e7bd450475c2186bf9bb7d524233d82dcc44d90d
canderomero <caande.romero@gmail.com>
2023-06-24 12:47:57 -0300

    "add new tech"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,15 @@ import { useState } from 'react'
 import Image from 'next/image'
 import profilePicture from '../../public/profilePicture.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
-import { faMoon } from '@fortawesome/free-regular-svg-icons';
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faMoon } from '@fortawesome/free-regular-svg-icons';
 import Link from 'next/link'
 import s from './page.module.css'
 import clsx from 'clsx';
 import html from '../../public/techs/html.png'
 import css from '../../public/techs/css.png'
 import javascript from '../../public/techs/javascript.png'
+import typescript from '../../public/techs/typescript.png'
 import react from '../../public/techs/react.png'
 import nextjs from '../../public/techs/nextjs.png'
 import github from '../../public/techs/github.png'
@@ -40,16 +41,16 @@ export default function Home() {
 
   },
   {
-      id: 4,
-      src: react,
-      title: "React",
-      style: "shadow-sky-400"
+    id: 4,
+    src: typescript,
+    title: "Typescript",
+    style: "shadow-sky-400"
 
   },
   {
       id: 5,
-      src: tailwind,
-      title: "Tailwind",
+      src: react,
+      title: "React",
       style: "shadow-sky-400"
 
   },
@@ -58,22 +59,28 @@ export default function Home() {
       src: nextjs,
       title: "Next JS",
       style: "shadow-gray-400"
-
   },
   {
       id: 7,
+      src: tailwind,
+      title: "Tailwind",
+      style: "shadow-sky-400"
+  },
+  {
+      id: 8,
       src: github,
       title: "Github",
       style: "shadow-gray-400"
-
   } 
 
   ]
   
   return (
     <div className={clsx('p-4', s.font, darkMode ? "bg-gray-900" : "bg-white" )}>
-        <div className={clsx('p-2 text-right', darkMode ? "text-white" : "")}>
-          <FontAwesomeIcon icon={faMoon} className='cursor-pointer' height={30} width={30} onClick={()=> setDarkMode(!darkMode)}/>
+        <div className={clsx('p-2 w-full inline-flex justify-end items-center cursor-pointer', darkMode ? "text-white" : "")}
+                        onClick={()=> setDarkMode(!darkMode)}>
+          {darkMode ? <p>Light mode</p> : <p>Dark mode</p>}
+          <FontAwesomeIcon icon={faMoon} height={30} width={30}/>
         </div>
         <div className="text-center pt-6 pb-4 px-10">
           <h1 className={clsx('text-3xl py-2 font-medium  md:text-4xl', darkMode ? "text-teal-400" : "text-teal-600")}>Candela Romero</h1>
@@ -83,16 +90,26 @@ export default function Home() {
         <div className={clsx('relative w-80 mx-auto border-2 rounded-full', darkMode ? "border-teal-400" : "border-teal-600")}>
           <Image src={profilePicture} alt='profilePicture' className='overflow-clip rounded-full'/>
         </div>
-        <div className={clsx('text-md text-center pt-4', darkMode ? "text-gray-400" : "text-gray-600" )}>
-          <Link href='https://www.linkedin.com/in/candelabromero/'> 
-            <div className='flex justify-center hover:text-gray-400'>
-              <p className='mr-1'>LinkedIn</p> 
-              <FontAwesomeIcon icon={faLinkedinIn} width={18} height={18} className='mt-1' />        
-            </div> 
-          </Link>
+        <div className={clsx('text-center mt-4 inline-flex bg-white rounded-md justify-center gap-x-2.5')}>
+          <div className='hover:scale-110 duration-500 text-blue-700'>
+            <Link href='https://www.linkedin.com/in/candelabromero/'> 
+              <FontAwesomeIcon icon={faLinkedinIn} width={35} className='h-[35px]' />        
+            </Link>
+          </div>
+          <div className='hover:scale-110 duration-500'>
+            <Link href='https://github.com/canderomero17'> 
+              <FontAwesomeIcon icon={faGithub} width={35} className='h-[35px]' />        
+            </Link>
+          </div> 
+          <div className='hover:scale-110 duration-500 text-sky-400'>
+            <Link href='mailto:caande.romero@gmail.com'> 
+              <FontAwesomeIcon icon={faEnvelope} width={35} className='h-[35px]' />        
+            </Link> 
+          </div> 
         </div>
-        <div className='pt-12'>
-        <div><p className={clsx('text-xl border-b-2 p-2 inline', darkMode ? "border-teal-400 text-white" : 'border-teal-600')}> Skills </p>
+        <div className='pt-12 px-7'>
+        <div>
+          <p className={clsx('text-xl border-b-2 p-2 inline', darkMode ? "border-teal-400 text-white" : 'border-teal-600')}> Skills </p>
         </div>
         <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center pb-8 py-10 px-12 sm:px-0'>          
           {techs.map(({id, src, title, style}) => (
